445ff9bc13482e6f9a091f4ae6685a2d4bce4c3c
Jonathan SM Zea <jonathan.a.zea@ieee.org>
2024-07-18 11:36:06 -0500

    :white_check_mark: complete LU descomposition code

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
-from .gaussian_elimination import eliminacion_gaussiana, descomposicion_LU, resolver_LU
+from .linear_sist_methods import (
+    eliminacion_gaussiana,
+    descomposicion_LU,
+    resolver_LU,
+    matriz_aumentada,
+    separar_m_aumentada,
+)
