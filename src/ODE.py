b9357bd4c01c12e2353588c8864848496d4d2df1
Jonathan SM Zea <jonathan.a.zea@ieee.org>
2024-08-01 16:55:50 -0500

    :sparkles: update Taylor method

--- src/ODE.py
+++ src/ODE.py
@@ -72,3 +72,61 @@ def ODE_euler(
         t += h
         ts.append(t)
     return ys, ts, h
+
+
+# ####################################################################
+from math import factorial
+
+
+def ODE_euler_nth(
+    *,
+    a: float,
+    b: float,
+    f: Callable[[float, float], float],
+    f_derivatives: list[Callable[[float, float], float]],
+    y_t0: float,
+    N: int,
+) -> tuple[list[float], list[float], float]:
+    """Solves (numerically) an ODE of the form
+        dy/dt = f(t, y)
+            y(t_0) = y_t0, a <= t_0 <= b
+    using the Taylor method with (m - 1)th derivatives for the N+1 points in the time range [a, b].
+
+    It generates N+1 mesh points with:
+        t_i = a + i*h, h = (a - b) / N,
+    where h is the step size.
+
+
+    ## Parameters
+    ``a``: initial time
+    ``b``: final time
+    ``f``: function of two variables ``t`` and ``y``
+    ``f_derivatives``: list of (m - 1)th derivatives of f
+    ``y_t0``: initial condition
+    ``N``: number of mesh points
+
+    ## Return
+    ``ys``: a list of the N+1 approximated values of y
+    ``ts``: a list of the N+1 mesh points
+    ``h``: the step size h
+
+    """
+    h = (b - a) / N
+    t = a
+    ts = [t]
+    ys = [y_t0]
+
+    for _ in range(N):
+        y = ys[-1]
+        T = f(t, y)
+        ders = [
+            h / factorial(m + 2) * mth_derivative(t, y)
+            for m, mth_derivative in enumerate(f_derivatives)
+        ]
+        T += sum(ders)
+        y += h * T
+        ys.append(y)
+
+        t += h
+        ts.append(t)
+    return ys, ts, h
